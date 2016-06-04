ddf1ea56843492338e710561ed3ca69f009edb82
Benjamin Lupton <b@lupton.cc>
2016-06-04 22:30:36 +0800

    fix travis

--- source/examples/example1.js
+++ source/examples/example1.js
@@ -19,7 +19,12 @@ joe.suite('example1', function (suite, test) {
 		// Only run if our environment supports this
 		if ( typeof window === 'undefined' ) {
 			// Attempt modification
-			joe.blah = "was set when it shouldn't have been"
+			try {
+				joe.blah = "was set when it shouldn't have been"
+			}
+			catch (err) {
+				// https://travis-ci.org/bevry/joe/jobs/135251737
+			}
 
 			// Test that the modifications were not successful
 			equal(joe.blah == null, true, `modification test, result: ${joe.blah}`)
